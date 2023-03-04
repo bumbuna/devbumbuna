71987a2ca6e6f3927913fd1cbbbcc90592737285
bumbuna <developer@devbumbuna.com>
2023-03-04 07:42:17 +0300

    install react-bootstrap package
    display under construction on homepage

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import WorkingOnIt from './components/WorkingOnIt';
 
 export const App = () => (
-  <div>
-    <h1>Under Active Development</h1>
-  </div>
+  <Container className='h-100'>
+    <WorkingOnIt />
+  </Container>
 );
---
+++ imports/ui/components/WorkingOnIt.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function WorkingOnIt() {
+    return (
+        <div className='d-flex h-100 flex-column justify-content-center'>
+            <div className='align-self-center'>
+                <img
+                    width={'300px'}
+                    src='assets/images/devbumbuna-logo.png' height={'300px'} />
+            </div>
+            <span className='h2 text-center'>Working on it</span>
+        </div>
+    )
+}
+
+export default WorkingOnIt
